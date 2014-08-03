c3749d87153139ba711603df3179e2be7ca97527
Brian Vaughn <brian@recurly.com>
2014-08-03 12:01:00 -0700

    removed a console.log and an html comment accidentally left in place.

--- tests/nested-object-helper.js
+++ tests/nested-object-helper.js
@@ -43,7 +43,6 @@ describe('NestedObjectHelper', function() {
         }
       });
 
-      console.log(keys);
       expect(keys).toContain('foo');
       expect(keys).toContain('deep.bar');
       expect(keys).toContain('deep.deeper.baz');
