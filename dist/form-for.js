cdaf9f9d7db2852a4ee6eba4861a0e865c2872ff
Brian Vaughn <brian@recurly.com>
2014-08-03 09:26:13 -0700

    Moved formForDebounce into the directives folder where it belonged.

--- dist/form-for.js
+++ dist/form-for.js
@@ -63,6 +63,70 @@ angular.module('formFor').directive('fieldLabel',
   });
 
 /**
+ * Angular introduced debouncing (via ngModelOptions) in version 1.3.
+ * As of the time of this writing, that version is still in beta.
+ * This component adds debouncing behavior for Angular 1.2.x.
+ * It is primarily intended for use with <input type=text> elements.
+ */
+angular.module('formFor').directive('formForDebounce', function($log, $timeout, FormForConfiguration) {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    priority: 99,
+    link: function($scope, $element, attributes, ngModelController) {
+      if (attributes.type === 'radio' || attributes.type === 'checkbox') {
+        $log.warn('formForDebounce should only be used with <input type=text> and <textarea> elements');
+
+        return;
+      }
+
+      var durationAttribute = attributes.formForDebounce;
+      var duration = FormForConfiguration.defaultDebounceDuration;
+      var debounce;
+
+      // Debounce can be configured for blur-only by passing a value of 'false'.
+      if (durationAttribute !== undefined) {
+        if (durationAttribute.toString() === 'false') {
+          duration = false;
+        } else {
+          durationAttribute = parseInt(durationAttribute);
+
+          if (!_.isNaN(durationAttribute)) {
+            duration = durationAttribute;
+          }
+        }
+      }
+
+      $element.unbind('input');
+
+      if (duration !== false) {
+        $element.bind('input', function() {
+          $timeout.cancel(debounce);
+
+          debounce = $timeout(function() {
+            $scope.$apply(function() {
+              ngModelController.$setViewValue($element.val());
+            });
+          }, duration);
+        });
+      }
+
+      $element.bind('blur', function() {
+        $scope.$apply(function() {
+          ngModelController.$setViewValue($element.val());
+        });
+      });
+
+      $scope.$on('$destroy', function() {
+        if (debounce) {
+          $timeout.cancel(debounce);
+        }
+      });
+    }
+  };
+});
+
+/**
  * For documentation please refer to the project wiki:
  * https://github.com/bvaughn/angular-form-for/wiki/API-Reference#formfor
  */
@@ -534,70 +598,6 @@ angular.module('formFor').service('FormForConfiguration',
       }
     };
   });
-
-/**
- * Angular introduced debouncing (via ngModelOptions) in version 1.3.
- * As of the time of this writing, that version is still in beta.
- * This component adds debouncing behavior for Angular 1.2.x.
- * It is primarily intended for use with <input type=text> elements.
- */
-angular.module('formFor').directive('formForDebounce', function($log, $timeout, FormForConfiguration) {
-  return {
-    restrict: 'A',
-    require: 'ngModel',
-    priority: 99,
-    link: function($scope, $element, attributes, ngModelController) {
-      if (attributes.type === 'radio' || attributes.type === 'checkbox') {
-        $log.warn('formForDebounce should only be used with <input type=text> and <textarea> elements');
-
-        return;
-      }
-
-      var durationAttribute = attributes.formForDebounce;
-      var duration = FormForConfiguration.defaultDebounceDuration;
-      var debounce;
-
-      // Debounce can be configured for blur-only by passing a value of 'false'.
-      if (durationAttribute !== undefined) {
-        if (durationAttribute.toString() === 'false') {
-          duration = false;
-        } else {
-          durationAttribute = parseInt(durationAttribute);
-
-          if (!_.isNaN(durationAttribute)) {
-            duration = durationAttribute;
-          }
-        }
-      }
-
-      $element.unbind('input');
-
-      if (duration !== false) {
-        $element.bind('input', function() {
-          $timeout.cancel(debounce);
-
-          debounce = $timeout(function() {
-            $scope.$apply(function() {
-              ngModelController.$setViewValue($element.val());
-            });
-          }, duration);
-        });
-      }
-
-      $element.bind('blur', function() {
-        $scope.$apply(function() {
-          ngModelController.$setViewValue($element.val());
-        });
-      });
-
-      $scope.$on('$destroy', function() {
-        if (debounce) {
-          $timeout.cancel(debounce);
-        }
-      });
-    }
-  };
-});
 
 /**
  * Organizes state management for form-submission and field validity.
