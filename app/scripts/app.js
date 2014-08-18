89c9ba62bd7eac1350ab866e2799e88be51822cd
bvaughn <brian.david.vaughn@gmail.com>
2014-08-17 23:33:45 -0700

    Rearranging

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,15 @@
 angular.module('formForDocumentation', ['formFor', 'ngRoute']).
   config(function($routeProvider) {
-    $routeProvider.when('/index', {templateUrl: 'views/index.html', controller: 'IndexController'});
+    $routeProvider.when('/index', {templateUrl: 'app/views/index.html', controller: 'IndexController'});
 
-    $routeProvider.when('/demo/simple-form', {templateUrl: 'views/simple-form.html', controller: 'SimpleFormDemoController'});
-    $routeProvider.when('/demo/advanced-form', {templateUrl: 'views/advanced-form.html', controller: 'AdvancedFormDemoController'});
-    $routeProvider.when('/demo/dynamic-dropdowns', {templateUrl: 'views/dynamic-dropdowns.html', controller: 'DynamicDropdownsDemoController'});
+    $routeProvider.when('/demo/simple-form', {templateUrl: 'app/views/simple-form.html', controller: 'SimpleFormDemoController'});
+    $routeProvider.when('/demo/advanced-form', {templateUrl: 'app/views/advanced-form.html', controller: 'AdvancedFormDemoController'});
+    $routeProvider.when('/demo/dynamic-dropdowns', {templateUrl: 'app/views/dynamic-dropdowns.html', controller: 'DynamicDropdownsDemoController'});
 
-    $routeProvider.when('/overview', {templateUrl: 'views/overview.html', controller: 'OverviewController'});
-    $routeProvider.when('/input-types', {templateUrl: 'views/input-types.html', controller: 'InputTypesController'});
-    $routeProvider.when('/validation-types', {templateUrl: 'views/validation-types.html', controller: 'ValidationTypesController'});
-    $routeProvider.when('/template-overrides', {templateUrl: 'views/template-overrides.html', controller: 'TemplateOverridesController'});
-    $routeProvider.when('/ie8-support', {templateUrl: 'views/ie8-support.html', controller: 'IE8SupportController'});
+    $routeProvider.when('/overview', {templateUrl: 'app/views/overview.html', controller: 'OverviewController'});
+    $routeProvider.when('/input-types', {templateUrl: 'app/views/input-types.html', controller: 'InputTypesController'});
+    $routeProvider.when('/validation-types', {templateUrl: 'app/views/validation-types.html', controller: 'ValidationTypesController'});
+    $routeProvider.when('/template-overrides', {templateUrl: 'app/views/template-overrides.html', controller: 'TemplateOverridesController'});
+    $routeProvider.when('/ie8-support', {templateUrl: 'app/views/ie8-support.html', controller: 'IE8SupportController'});
     $routeProvider.otherwise({redirectTo: '/index'});
   });
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var connect = require('gulp-connect');
 
 gulp.task('connect', function() {
   connect.server({
-    root: 'app',
     livereload: true
   });
 });
