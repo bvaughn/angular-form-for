11008f5b20baf68d7247eed83cf41cda312b4af7
bvaughn <brian.david.vaughn@gmail.com>
2014-09-07 10:30:48 -0700

    Resole #42 Updated custom field docs to reference FieldHelper.

--- app/samples/custom-field.js
+++ app/samples/custom-field.js
@@ -1,15 +1,20 @@
 angular.module('myAngularModule').directive('customField',
-    function() {
-      return {
-        require: '^formFor',
-        template: '<input type="text" ng-model="model.bindable" ng-disabled="disabledByForm" ng-debounce>',
-        scope: {
-          attribute: '@',
-          disabledByForm: '@'
-        },
-        link: function($scope, $element, $attributes, formForController) {
-          $scope.model = formForController.registerFormField($scope, $scope.attribute);
-        }
-      };
-    }
-  );
+  function(FieldHelper) {
+    return {
+      restrict: 'E',
+      require: '^formFor',
+      template: '<div>' +
+                '  <p ng-if="model.error" ng-bind="model.error"></p>' +
+                '  <input type="text" ng-model="model.bindable" ng-disabled="model.disabled" ng-debounce>' +
+                '</div>',
+      scope: {
+        attribute: '@'
+      },
+      link: function($scope, $element, $attributes, formForController) {
+        // It is not necessary to use FieldHelper, but it simplifies registration.
+        // $scope will have a 'model' attribute after this method has executed.
+        FieldHelper.manageFieldRegistration($scope, formForController);
+      }
+    };
+  }
+);
