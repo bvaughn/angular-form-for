ae7d38f2fedf68ab4b33d979d2370f0b01ec2fe4
bvaughn <brian.david.vaughn@gmail.com>
2015-03-06 09:04:48 -0800

    All tests passing. Huzzah

--- tests/services/string-util.js
+++ tests/services/string-util.js
@@ -3,32 +3,26 @@ describe('StringUtil', function() {
 
   beforeEach(module('formFor'));
 
-  var StringUtil;
-
-  beforeEach(inject(function ($injector) {
-    StringUtil = $injector.get('StringUtil');
-  }));
-
   describe('StringUtil', function() {
     it('should gracefully handle null and empty strings', function() {
-      expect(StringUtil.humanize(null)).toEqual('');
-      expect(StringUtil.humanize(undefined)).toEqual('');
-      expect(StringUtil.humanize('')).toEqual('');
+      expect(formFor.StringUtil.humanize(null)).toEqual('');
+      expect(formFor.StringUtil.humanize(undefined)).toEqual('');
+      expect(formFor.StringUtil.humanize('')).toEqual('');
     });
 
     it('should convert snake-case variables to humanized strings', function() {
-      expect(StringUtil.humanize('snake_case')).toEqual('Snake Case');
-      expect(StringUtil.humanize('snake_case_too')).toEqual('Snake Case Too');
+      expect(formFor.StringUtil.humanize('snake_case')).toEqual('Snake Case');
+      expect(formFor.StringUtil.humanize('snake_case_too')).toEqual('Snake Case Too');
     });
 
     it('should convert camel-case variables to humanized strings', function() {
-      expect(StringUtil.humanize('camelCase')).toEqual('Camel Case');
-      expect(StringUtil.humanize('camelCaseToo')).toEqual('Camel Case Too');
+      expect(formFor.StringUtil.humanize('camelCase')).toEqual('Camel Case');
+      expect(formFor.StringUtil.humanize('camelCaseToo')).toEqual('Camel Case Too');
     });
 
     it('should not convert already-humanized strings', function() {
-      expect(StringUtil.humanize('Word')).toEqual('Word');
-      expect(StringUtil.humanize('Humanized String')).toEqual('Humanized String');
+      expect(formFor.StringUtil.humanize('Word')).toEqual('Word');
+      expect(formFor.StringUtil.humanize('Humanized String')).toEqual('Humanized String');
     });
   });
 });
