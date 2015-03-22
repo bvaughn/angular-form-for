d167858dbb3be03e29504b4fac46f3cf68a5a276
bvaughn <brian.david.vaughn@gmail.com>
2015-03-22 08:17:25 -0700

    Moved Bower dependencies over to NPM for easier Travis integration

--- karma.conf.js
+++ karma.conf.js
@@ -12,11 +12,11 @@ module.exports = function(config) {
     ngHtml2JsPreprocessor: {
     },
     files: [
-      'bower_components/jquery/dist/jquery.js',
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/jasmine-object-matchers/dist/jasmine-object-matchers.js',
-      'bower_components/jasmine-promise-matchers/dist/jasmine-promise-matchers.js',
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/jasmine-object-matchers/dist/jasmine-object-matchers.js',
+      'node_modules/jasmine-promise-matchers/dist/jasmine-promise-matchers.js',
       'dist/form-for.js',
       'tests/**/*.js',
       'templates/**/*.html'
