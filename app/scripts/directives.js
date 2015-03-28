95f3dcae67d01de8e760439482139caf38e6f59a
bvaughn <brian.david.vaughn@gmail.com>
2015-03-28 12:04:28 -0700

    Updated to use 3.0.0 release

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -89,14 +89,14 @@ angular.module('formForDocumentation').directive('resetFieldButton', function() 
 });
 
 angular.module('formForDocumentation').value('currentTemplates', {
-  key: 'material'
+  key: 'default'
 });
 
 angular.module('formForDocumentation').directive('templateToggler', function($ocLazyLoad, $state, $stateParams, currentTemplates) {
   var map = {};
-  map['bootstrap'] = ['formFor.bootstrapTemplates', '/offline/form-for.bootstrap-templates.js'];
-  map['default'] = ['formFor.defaultTemplates', '/offline/form-for.default-templates.js'];
-  map['material'] = ['formFor.materialTemplates', '/offline/form-for.material-templates.js'];
+  map['bootstrap'] = ['formFor.bootstrapTemplates', 'https://rawgit.com/bvaughn/angular-form-for/3.0.0/dist/form-for.bootstrap-templates.js'];
+  map['default'] = ['formFor.defaultTemplates', 'https://rawgit.com/bvaughn/angular-form-for/3.0.0/dist/form-for.default-templates.js'];
+  map['material'] = ['formFor.materialTemplates', 'https://rawgit.com/bvaughn/angular-form-for/3.0.0/dist/form-for.material-templates.js'];
 
   return {
     restrict: 'E',
