792184f5bbc920e10d769f9ad179e236b170756e
bvaughn <brian.david.vaughn@gmail.com>
2015-05-09 17:23:49 -0700

    Resolves #108: ngIncluded template partials (e.g. select-field/_select.html) now recompiled before being replaced. This fixes an issue w/ jQlite that caused these partials to become disconnected from the parent directive .

--- dist/form-for.js
+++ dist/form-for.js
@@ -750,25 +750,31 @@ var formFor;
 })(formFor || (formFor = {}));
 var formFor;
 (function (formFor) {
+    var $compile_;
     /**
      * Adds the ability to replace an ngInclude element with its template content.
      */
     var FormForIncludeReplaceDirective = (function () {
-        function FormForIncludeReplaceDirective() {
+        /* @ngInject */
+        function FormForIncludeReplaceDirective($compile) {
             this.require = 'ngInclude';
             this.restrict = 'A'; /* optional */
+            $compile_ = $compile;
         }
+        FormForIncludeReplaceDirective.$inject = ["$compile"];
         /* @ngInject */
         FormForIncludeReplaceDirective.prototype.link = function ($scope, $element, $attributes) {
-            $element.replaceWith($element.children());
+            var html = $element.prop('innerHTML');
+            var compiled = $compile_(html)($scope);
+            $element.replaceWith(compiled);
         };
         FormForIncludeReplaceDirective.prototype.link.$inject = ["$scope", "$element", "$attributes"];
         return FormForIncludeReplaceDirective;
     })();
     formFor.FormForIncludeReplaceDirective = FormForIncludeReplaceDirective;
-    angular.module('formFor').directive('formForIncludeReplace', function () {
-        return new FormForIncludeReplaceDirective();
-    });
+    angular.module('formFor').directive('formForIncludeReplace', ["$compile", function ($compile) {
+        return new FormForIncludeReplaceDirective($compile);
+    }]);
 })(formFor || (formFor = {}));
 /// <reference path="../../definitions/angular.d.ts" />
 var formFor;
