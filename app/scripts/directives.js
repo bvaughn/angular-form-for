48bfe1dc0d87551c590190595e9a502b1e95d807
bvaughn <brian.david.vaughn@gmail.com>
2015-07-12 08:46:35 -0700

    Updated docs site to 4.0.6 release and added info about new FormForConfiguration.setHelpIcon method.

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -114,7 +114,7 @@ angular.module('formForDocumentation').value('currentTemplates', {
 });
 
 angular.module('formForDocumentation').directive('templateToggler', function($ocLazyLoad, $state, $stateParams, currentTemplates) {
-  var baseUrl = 'http://rawgit.com/bvaughn/angular-form-for/4.0.4/dist/';
+  var baseUrl = 'http://rawgit.com/bvaughn/angular-form-for/4.0.6/dist/';
   var map = {};
   map['bootstrap'] = ['formFor.bootstrapTemplates', baseUrl + 'form-for.bootstrap-templates.js'];
   map['default'] = ['formFor.defaultTemplates', baseUrl + 'form-for.default-templates.js'];
