7ae107ef7b913a6112ece42fc4a81c3dfae77412
bvaughn <brian.david.vaughn@gmail.com>
2015-08-02 08:21:59 -0700

    Updated gh-pages to 4.1.1 release

--- built/angular-form-for-docs.js
+++ built/angular-form-for-docs.js
@@ -799,7 +799,7 @@ angular.module('formForDocumentation').value('currentTemplates', {
 });
 
 angular.module('formForDocumentation').directive('templateToggler', ["$ocLazyLoad", "$state", "$stateParams", "currentTemplates", function($ocLazyLoad, $state, $stateParams, currentTemplates) {
-  var baseUrl = 'http://rawgit.com/bvaughn/angular-form-for/4.1.0/dist/';
+  var baseUrl = 'http://rawgit.com/bvaughn/angular-form-for/4.1.1/dist/';
   var map = {};
   map['bootstrap'] = ['formFor.bootstrapTemplates', baseUrl + 'form-for.bootstrap-templates.js'];
   map['default'] = ['formFor.defaultTemplates', baseUrl + 'form-for.default-templates.js'];
