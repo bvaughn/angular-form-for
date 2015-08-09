f2210f6c250447e85286fe946932a5876cd399b0
Nick Guletskii <nick@nickguletskii.com>
2015-08-10 00:03:34 +0300

    Attempted to fix broken URLs
    Attempted to fix broken URLs by adding a base tag and using
    protocol-relative URLs. Please note that this commit doesn't contain
    the built scripts, so it should be followed by a gulp build.

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -114,7 +114,7 @@ angular.module('formForDocumentation').value('currentTemplates', {
 });
 
 angular.module('formForDocumentation').directive('templateToggler', function($ocLazyLoad, $state, $stateParams, currentTemplates) {
-  var baseUrl = 'http://rawgit.com/bvaughn/angular-form-for/4.1.0/dist/';
+  var baseUrl = '//rawgit.com/bvaughn/angular-form-for/4.1.0/dist/';
   var map = {};
   map['bootstrap'] = ['formFor.bootstrapTemplates', baseUrl + 'form-for.bootstrap-templates.js'];
   map['default'] = ['formFor.defaultTemplates', baseUrl + 'form-for.default-templates.js'];
